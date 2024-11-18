6379bf1d290b6362c29abf141fa8c0607b12b19e
Parsa <92217944+ParsaMollaie@users.noreply.github.com>
2024-11-18 19:41:29 +0330

    fix(bug): fix next config bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: {
+    source: '/api/(.*)',
+  },
+  headers: [
+    {
+      key: 'Access-Control-Allow-Origin',
+      value: 'https://dev-flow-sable.vercel.app',
+    },
+    {
+      key: 'Access-Control-Allow-Methods',
+      value: 'GET, POST, OPTIONS',
+    },
+    {
+      key: 'Access-Control-Allow-Headers',
+      value: 'Content-Type',
+    },
+  ],
   experimental: {
     serverActions: true,
     mdxRs: true,
-    serverComponentsExternalPackages: ["mongoose"],
+    serverComponentsExternalPackages: ['mongoose'],
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "*",
+        protocol: 'https',
+        hostname: '*',
       },
       {
-        protocol: "http",
-        hostname: "*",
+        protocol: 'http',
+        hostname: '*',
       },
     ],
   },
